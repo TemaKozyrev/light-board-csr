292510dd08e55e00021e7aec1c7d5216865d831d
TemaKozyrev <tema@tema.ws>
2017-03-30 19:29:56 +0300

    header

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -27,16 +27,6 @@ $(document).ready(function() {
              $('#modal_content').remove();
      });
 
-     $('#open_registration_modal').click(function () {
-         overlay.fadeIn(400,
-             function(){
-                 modal.load('/html/registration.html');
-                 modal
-                     .css('display', 'block')
-                     .animate({opacity: 1, top: '50%'}, 200);
-             });
-     });
-
      $.get( "http://localhost:3000/account/info", function( data ) {
         if (data.error) {
             $('#user').load('/html/login.html')
