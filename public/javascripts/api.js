80c6413b6fa20fef2590dc970666038c2f38dcf8
Артемий Козырев <tema@tema.ws>
2017-04-16 14:37:18 +0300

    index page api

---
+++ public/javascripts/api.js
@@ -0,0 +1,50 @@
+function _api() {
+
+  this.getSortOffers = function(callback) {
+    $.ajax({
+        type: 'GET',
+        url: "http://localhost:3000/",
+        data: {},
+        success: function (data) {
+            callback(data);
+        }
+    });
+  };
+
+  this.postUser = function(values, callback) {
+    $.ajax({
+        type: 'POST',
+        url: "http://localhost:3000/account/login",
+        data: {
+            "username": values.username,
+            "password": values.password
+        },
+        success: function (data) {
+            if (!data.error) {
+                callback(data)
+            } else
+                return false
+        },
+    });
+  };
+
+  this.logOut = function(callback) {
+    $.ajax({
+        type: 'GET',
+        url: "http://localhost:3000/account/logout",
+        success: function (data) {
+            callback()
+        },
+    });
+  };
+
+  this.getUserInfo = function(cb, err_cb) {
+    $.get( "http://localhost:3000/account/info", function( data ) {
+       if (data.error) {
+           err_cb();
+       } else {
+           cb(data);
+       }
+   });
+  }
+}
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,42 +1,51 @@
-$(document).ready(function() { 
-    var overlay = $('#overlay'); 
-    var open_modal = $('.open_modal'); 
-    var close = $('#modal_close, #overlay'); 
-    var modal = $('#modal_form'); 
+var api = new _api();
 
-     open_modal.click( function(event){ 
-         event.preventDefault(); 
-         var div = $(this).attr('href'); 
-         overlay.fadeIn(400, 
-             function(){ 
+$(document).ready(function() {
+    var overlay = $('#overlay');
+    var open_modal = $('.open_modal');
+    var close = $('#modal_close, #overlay');
+    var modal = $('#modal_form');
+
+     open_modal.click( function(event){
+         event.preventDefault();
+         var div = $(this).attr('href');
+         overlay.fadeIn(400,
+             function(){
                  modal.append("<div id='modal_content'><img src='/images/elep.jpg' class='item-img'><p>test/test</p><p>test-test</p></div>");
-                 modal 
-                     .css('display', 'block') 
-                     .animate({opacity: 1, top: '50%'}, 200); 
+                 modal
+                     .css('display', 'block')
+                     .animate({opacity: 1, top: '50%'}, 200);
          });
      });
 
-     close.click( function(){ 
-            modal 
-             .animate({opacity: 0, top: '45%'}, 200, 
-                 function(){ 
+     close.click( function(){
+            modal
+             .animate({opacity: 0, top: '45%'}, 200,
+                 function(){
                      $(this).css('display', 'none');
-                     overlay.fadeOut(400); 
+                     overlay.fadeOut(400);
                  }
              );
              $('#modal_content').remove();
              $('.registration_form').remove();
      });
 
-     $.get( "http://localhost:3000/account/info", function( data ) {
-        if (data.error) {
-            $('#user').load('/html/login.html');
-        } else {
-            $('#user').load('/html/user_info.html', function () {
-                $('.username').append('<a>' + data + '</a>')
-            });
-        }
-    });
+     api.getSortOffers(function (data) {
+       data.cat.forEach(function (item) {
+         $("#catrgory-list").append('<li class="category"><a>' + item.name + '</a></li>');
+       });
+       data.offers.forEach(function (item) {
+         $("#offer-list").append('<li class="flex-offer"><p>' + item.title + '</p><p>' + item.description + '</p></li>')
+       })
+     });
+
+     api.getUserInfo(function (name) {
+       $('#user').load('/html/user_info.html', function () {
+           $('.username').append('<a href="http://localhost:3000/html/profile.html">' + name + '</a>')
+       });
+     }, function () {
+       $('#user').load('/html/login.html');
+     })
 
     $(document).on( "click", ".registration", function(event) {
         event.preventDefault();
@@ -49,6 +58,7 @@ $(document).ready(function() {
             });
     });
 
+    //submit login form
     $(document).on("submit", ".login_form", function (event, data) {
         event.preventDefault();
         var $inputs = $('.login_form :input');
@@ -58,34 +68,20 @@ $(document).ready(function() {
             values[this.name] = $(this).val();
         });
 
-        $.ajax({
-            type: 'POST',
-            url: "http://localhost:3000/account/login",
-            data: {
-                "username": values.username,
-                "password": values.password
-            },
-            success: function (data) {
-                if (!data.error) {
-                    $('#user.user-form-flex').remove();
-                    $('#user').load('/html/user_info.html', function () {
-                        $('.username').append('<a>' + data + '</a>')
-                    });
-                } else
-                    alert(data.error)
-            },
+        api.postUser(values, function(name) {
+          $('#user.user-form-flex').remove();
+          $('#user').load('/html/user_info.html', function () {
+              $('.username').append('<a>' + name + '</a>')
+          });
         });
     });
 
+    //logout
     $(document).on( "click", ".logout", function(event) {
         event.preventDefault();
-        $.ajax({
-            type: 'GET',
-            url: "http://localhost:3000/account/logout",
-            success: function (data) {
-                $('#user.user-form-flex').remove();
-                $('#user').load('/html/login.html');
-            },
-        });
+        api.logOut(function() {
+          $('#user.user-form-flex').remove();
+          $('#user').load('/html/login.html');
+        })
     });
 });
